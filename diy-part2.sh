7f87282283a8c1df91b7ee6f1760514eb54ce25a
璀璨梦星 <157385609+cuicanmengxing@users.noreply.github.com>
2024-07-13 09:00:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,11 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
+# 替换主题
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+# 替换默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Modify hostname
+# 替换默认主机名
 sed -i 's/OpenWrt/Redmi_AX5/g' package/base-files/files/bin/config_generate
