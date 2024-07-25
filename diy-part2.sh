f0d8b54e29c5d741dc9d507b1f9a04d0b0771e84
cuicanmengxing <cuicanmengxing@163.com>
2024-07-25 17:56:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,24 +1,3 @@
-#!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-rm -rf ./package/custom_packages
-mkdir -p ./package/custom_packages
-
-# Modify default IP
-# sed -i 's/192.168.1.1/10.10.0.253/g' package/base-files/files/bin/config_generate
-
-# Modify default passwd
-# sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
-
 # Modify hostname
 sed -i 's/OpenWrt/Redmi_AX5/g' package/base-files/files/bin/config_generate
 
@@ -32,6 +11,3 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./pack
 
 #修改连接数
 sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
-
-
-
