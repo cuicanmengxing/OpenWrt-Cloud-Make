aea6ca5af82ca0e955c5fe3ae191b33c127450f1
cuicanmengxing <cuicanmengxing@163.com>
2024-08-01 17:22:06 +0800

    更新

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,5 +6,6 @@ rm -rf ./feeds/luci/themes/luci-theme-argon-mod
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/custom_packages/luci-theme-argon
 rm -rf ./feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./package/custom_packages/luci-app-argon-config
+git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
 #修改连接数
 sed -i 's/16384/65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
